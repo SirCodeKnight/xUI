1f2ad87d69de9fa34dc8ca4c531431948a899617
SirCodeKnight <gaurangatamuly25@gmail.com>
2025-01-09 16:37:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional: Add any interactive JavaScript functionality here
+// For example, smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
